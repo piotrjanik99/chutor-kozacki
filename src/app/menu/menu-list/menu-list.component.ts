71f9810c092f62304b4f373d76e381600409efdd
Piotr <azaer99@gmail.com>
2024-12-01 14:22:15 +0100

    menu finalization, adjusting resposivity, adding descriptions

--- src/app/menu/menu-list/menu-list.component.ts
+++ src/app/menu/menu-list/menu-list.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 import {MenuListItemComponent} from "./menu-list-item/menu-list-item.component";
-import {appetizers, desserts, hotSnacks, kidsMenu, mainCourses, sides, soups} from "../../constants/menu.constants";
+import {
+  appetizers, bottledBeer, bourbon, brandy, cocktails, coldDrinks,
+  desserts, gin,
+  hotDrinks,
+  hotSnacks,
+  kidsMenu, liquors,
+  mainCourses, rum,
+  sides,
+  soups, tapBeer, tequila, vodkaClear, vodkaCraft, wermut, whisky
+} from "../../constants/menu.constants";
 
 @Component({
   selector: 'app-menu-list',
@@ -19,4 +28,19 @@ export class MenuListComponent {
   protected readonly desserts = desserts;
   protected readonly kidsMenu = kidsMenu;
   protected readonly sides = sides;
+  protected readonly hotDrinks = hotDrinks;
+  protected readonly coldDrinks = coldDrinks;
+  protected readonly tapBeer = tapBeer;
+  protected readonly bottledBeer = bottledBeer;
+  protected readonly cocktails = cocktails;
+  protected readonly whisky = whisky;
+  protected readonly bourbon = bourbon;
+  protected readonly brandy = brandy;
+  protected readonly rum = rum;
+  protected readonly gin = gin;
+  protected readonly liquors = liquors;
+  protected readonly tequila = tequila;
+  protected readonly wermut = wermut;
+  protected readonly vodkaCraft = vodkaCraft;
+  protected readonly vodkaClear = vodkaClear;
 }
