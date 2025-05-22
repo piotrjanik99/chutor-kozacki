5d902a716314b55de89b34eefedd6ca185efd5fc
azaer <azaer99@gmail.com>
2025-05-22 15:33:54 +0200

    changed menu design, added photos, changed gallery design from accordions to nav tabs

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -18,9 +18,74 @@ export class GalleryComponent implements AfterViewInit {
   @ViewChild('imageModal') imageModal!: TemplateRef<any>;
   modalService = inject(NgbModal);
 
-  dishImages = ['dish1.png', 'dish2.png', 'dish3.png', 'dish4.png'];
-  venueImages = ['staff1.png', 'staff2.png', 'staff3.png', 'bg.jpg', 'bg2.jpg'];
-  eventImages = ['event1.png', 'event2.png', 'event3.png'];
+  dishImages = [
+    'dish1.png',
+    'dish2.png',
+    'dish3.png',
+    'dish4.png',
+    'gallery/dish1.jpeg',
+    'gallery/dish2.jpeg',
+    'gallery/dish4.jpeg',
+    'gallery/dish5.jpeg',
+    'gallery/dish6.jpeg',
+    'gallery/dish7.jpg',
+    'gallery/dish8.jpg',
+    'gallery/dish9.jpg',
+    'gallery/dish10.jpg'];
+  venueImages = [
+    'gallery/venue3.jpeg',
+    'gallery/venue5.jpg',
+    'gallery/venue6.jpg',
+    'gallery/venue7.jpg',
+    'gallery/venue8.jpg',
+    'gallery/venue9.jpg',
+    'gallery/venue10.jpg',
+    'gallery/venue11.jpg',
+    'gallery/venue12.jpg',
+    'gallery/venue13.jpg',
+    'gallery/venue14.jpg',
+    'gallery/venue15.jpg',
+    'gallery/venue16.jpg',
+    'gallery/venue17.jpg',
+    'gallery/venue18.jpg',
+    'gallery/venue19.jpg',
+    'gallery/venue20.jpg',
+    'gallery/venue21.jpg',];
+  eventImages = [
+    'gallery/event1.jpeg',
+    'gallery/event2.jpeg',
+    'gallery/event3.jpeg',
+    'gallery/event4.jpeg',
+    'gallery/event5.jpeg',
+    'gallery/event6.jpeg',
+    'gallery/event7.jpeg',
+    'gallery/event8.jpeg',
+    'gallery/event9.jpeg',
+    'gallery/event10.jpeg',
+    'gallery/event11.jpeg',
+    'gallery/event12.jpeg',
+    'gallery/event13.jpeg',
+    'gallery/event14.jpeg',
+    'gallery/event15.jpeg',
+    'gallery/event16.jpeg',
+    'gallery/event17.jpeg',
+    'gallery/event18.jpeg',
+    'gallery/event19.jpeg',
+    'gallery/event20.jpeg',
+    'gallery/event21.jpeg',
+    'gallery/event22.jpeg',
+    'gallery/event23.jpeg',
+    'gallery/event24.jpeg',
+    'gallery/event25.jpeg',
+    'gallery/event26.jpeg',
+    'gallery/event27.jpeg',
+    'gallery/event28.jpg',
+    'gallery/event29.jpg',
+    'gallery/event30.jpg',
+    'gallery/event31.jpg',
+    'gallery/event32.jpg',
+    'gallery/event33.jpg',
+  ];
 
   currentImage = '';
   currentCategory = '';
@@ -32,6 +97,16 @@ export class GalleryComponent implements AfterViewInit {
         block: 'start'
       });
     }, 100);
+  }
+
+  accordionExpanded = {
+    dishes: true, // starts expanded
+    venue: false,
+    events: false
+  };
+
+  onAccordionToggle(section: 'dishes' | 'venue' | 'events') {
+    this.accordionExpanded[section] = !this.accordionExpanded[section];
   }
 
   getPath(image: string): string {
--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
-import { AppComponent } from '../app.component';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { MenuComponent } from '../menu/menu.component';
 import { ContactComponent } from '../contact/contact.component';
@@ -16,14 +15,12 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
     MenuComponent,
     NgbModule,
     ContactComponent,
-    AppComponent
   ],
   exports: [
     HeaderComponent,
     CarouselComponent,
     MenuComponent,
     ContactComponent,
-    AppComponent
   ]
 })
 export class AppModule { }
